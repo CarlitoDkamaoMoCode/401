a8b79ac1ef725b985ab4cba90e812c2967aa3b65
CarlitoDkamaoMoCode <carlsimporios096@gmail.com>
2024-09-06 08:45:22 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Carl Simporios';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'A skilled professional experienced in developing, testing, debugging, and maintaining software code to build functional applications and systems. Proficient in various programming languages, algorithms, data structures, and software development practices.';
+export const ABOUT_TEXT_2 =
+  'Responsible for editing and producing video content from start to finish, collaborating closely with cross-functional teams to design, implement, and deliver high-quality videos that align with business goals and creative vision.';
+export const ABOUT_TEXT_3 =
+  'Versatile video editor skilled in all aspects of video production, capable of handling projects from initial concept to final delivery. Possesses a deep understanding of editing software, visual effects, color grading, and audio mixing techniques.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Video Editor with 12+ years of experience creating high-quality videos and visual content using industry-standard editing tools and software. Extensive knowledge of video production techniques, including editing, motion graphics, color correction, and sound design.';
+export const RESUME_TEXT_2 =
+  'Video Editor with 12+ years of experience crafting engaging video content using advanced editing software and techniques. Skilled in storytelling, motion graphics, color grading, and sound design to produce compelling visual narratives.';
